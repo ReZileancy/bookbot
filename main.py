085dc1f572aefaa4b4cc67776cdcc08e857ec963
ReZileancy <wkr206@gmail.com>
2025-05-01 10:36:57 -0700

    read book

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(filePath):
+    with open(filePath) as file:
+        fileContents = file.read()
+    return fileContents
+
+def main():
+    get_book_text("books/frankenstein.txt")
+    
