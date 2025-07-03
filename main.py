b2fe43da5b5233482b534e4c350f52aba4aea79a
Warren <72475445+ReZileancy@users.noreply.github.com>
2025-07-03 10:18:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from stats import get_book_text,get_num_words,countChar,sortedDict
 import sys
 
 def main():
+    if len(sys.argv) == 2:
+        book_text = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print("Analyzing book found at " + book_text + " ...")
     print("----------- Word Count ----------")
     get_num_words()
     
-    book_text = get_book_text("books/frankenstein.txt")
     freq = countChar(book_text)
     sorted_chars = sortedDict(freq)
     
